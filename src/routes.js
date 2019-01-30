9ff7b10f817309ecac2c3062f4dd796b1a59506a
beatrizf13 <azz.felix7@gmail.com>
2019-01-30 19:10:48 -0300

    config multer

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
-const routes = require("express").Router();
+const routes = require('express').Router();
+const multer = require('multer');
+const multerConfig = require('./config/multer');
 
-routes.get("/", (req, res) => {
-  return res.json({
-    name: "nodejs-upload-files",
-    version: "1.0.0"
-  });
+routes.get('/', (req, res) => res.json({
+  name: 'nodejs-upload-files',
+  version: '1.0.0',
+}));
+
+routes.post('/posts', multer(multerConfig).single('file'), (req, res) => {
+  console.log(req.file);
+  return res.send(200);
 });
 
 module.exports = routes;
