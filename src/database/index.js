91952c97a6f76b91b935fe919473b8ff06cc451e
beatrizf13 <azz.felix7@gmail.com>
2019-01-30 22:14:25 -0300

    environment variables

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  'mongodb://beatrizf13:beatrizf13@ds145573.mlab.com:45573/upload-files',
+  process.env.MONGO_URL,
   { useNewUrlParser: true },
 );
 
