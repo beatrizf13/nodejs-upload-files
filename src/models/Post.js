7d2cf9b448ec1b1ba7639f5a4aaee5271db4021d
beatrizf13 <azz.felix7@gmail.com>
2019-01-30 22:37:49 -0300

    excluding local and cloud files

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,4 +1,10 @@
+const aws = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
 const mongoose = require('../database');
+
+const s3 = new aws.S3();
 
 const PostSchema = new mongoose.Schema({
   name: String,
@@ -16,6 +22,19 @@ PostSchema.pre('save', function () {
   if (!this.url) {
     this.url = `${process.env.APP_URL}/files/${this.key}`;
   }
+});
+
+// eslint-disable-next-line func-names
+PostSchema.pre('remove', function () {
+  if (process.env.STORAGE_TYPE === 's3') {
+    return s3
+      .deleteObject({
+        Bucket: process.env.AWS_S3_BUCKET,
+        Key: this.key,
+      })
+      .promise();
+  }
+  return promisify(fs.unlink)(path.resolve(__dirname, '..', '..', 'tmp', 'uploads', this.key));
 });
 
 const Post = mongoose.model('Post', PostSchema);
