73e9183f8ce818e3cce01806d4ed747e166737b1
beatrizf13 <azz.felix7@gmail.com>
2019-01-30 21:37:41 -0300

    storing files in the cloud with aws s3

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,14 +1,31 @@
 const multer = require('multer');
 const path = require('path');
 const crypto = require('crypto');
+const aws = require('aws-sdk');
+const multerS3 = require('multer-s3');
 
-module.exports = {
-  dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-  storage: multer.diskStorage({
+const storageTypes = {
+  local: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'));
     },
     filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err);
+
+        // eslint-disable-next-line no-param-reassign
+        file.key = `${hash.toString('hex')}-${file.originalname}`;
+
+        cb(null, file.key);
+      });
+    },
+  }),
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket: 'beatrizf13-upload-file',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (req, file, cb) => {
       crypto.randomBytes(16, (err, hash) => {
         if (err) cb(err);
 
@@ -18,6 +35,11 @@ module.exports = {
       });
     },
   }),
+};
+
+module.exports = {
+  dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+  storage: storageTypes.s3,
   limits: {
     fileSize: 2 * 1024 * 1024,
   },
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 const routes = require('express').Router();
 const multer = require('multer');
-const multerConfig = require('./config/multer');
+const multerConfig = require('../config/multer');
 
-const Post = require('./models/Post');
+const Post = require('../models/Post');
 
 routes.get('/', (req, res) => res.json({
   name: 'nodejs-upload-files',
@@ -10,13 +12,15 @@ routes.get('/', (req, res) => res.json({
 }));
 
 routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
-  const { originalname: name, size, key } = req.file;
+  const {
+    originalname: name, size, key, location: url = '',
+  } = req.file;
 
   const post = await Post.create({
     name,
     size,
     key,
-    url: '',
+    url,
   });
 
   return res.send(post);
