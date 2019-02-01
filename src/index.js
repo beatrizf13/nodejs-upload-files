a1d98f1945e07ba3f3101177216bec8b2f537636
beatrizf13 <azz.felix7@gmail.com>
2019-01-31 22:26:59 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
